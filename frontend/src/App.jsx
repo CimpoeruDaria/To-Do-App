938640055b6df4261ba7dbccf9d159bde3db5651
CimpoeruDaria <dariamcimpoeru@gmail.com>
2026-07-27 13:58:06 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Login from './Login'
 import Register from './Register'
+import ForgotPassword from './ForgotPassword'
+import MainPage from './MainPage'
 import './App.css'
 
 function App() {
@@ -9,12 +11,12 @@ function App() {
 
   const handleLoginSuccess = (userData) => {
     setUser(userData)
-    setCurrentPage('dashboard')
+    setCurrentPage('mainpage')
   }
 
   const handleRegisterSuccess = (userData) => {
     setUser(userData)
-    setCurrentPage('dashboard')
+    setCurrentPage('mainpage')
   }
 
   const handleLogout = () => {
@@ -24,12 +26,35 @@ function App() {
 
   return (
     <div className="app-container">
+      {/* 1. Pagina de Login */}
       {currentPage === 'login' && (
-        <Login onNavigate={setCurrentPage} onLoginSuccess={handleLoginSuccess} />
+        <Login 
+          onNavigate={setCurrentPage} 
+          onLoginSuccess={handleLoginSuccess} 
+        />
       )}
       
+      {/* 2. Pagina de Register */}
       {currentPage === 'register' && (
-        <Register onNavigate={setCurrentPage} onRegisterSuccess={handleRegisterSuccess} />
+        <Register 
+          onNavigate={setCurrentPage} 
+          onRegisterSuccess={handleRegisterSuccess} 
+        />
+      )}
+
+      {/* 3. Pagina de Recuperare Parolă */}
+      {currentPage === 'forgot-password' && (
+        <ForgotPassword 
+          onNavigate={setCurrentPage} 
+        />
+      )}
+
+      {/* 4. Pagina Principală (Jurnalul) */}
+      {currentPage === 'mainpage' && (
+        <MainPage 
+          user={user} 
+          onLogout={handleLogout} 
+        />
       )}
     </div>
   )
